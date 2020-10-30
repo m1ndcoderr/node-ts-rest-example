690b861101fd9801b919463151fef478d14bb0c7
Evgeniy Khodzitskiy <m1ndcoderr@gmail.com>
2020-10-30 10:17:15 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ const studentController = new StudentController(studentService)
 
 createServer(
   async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+
     await studentController.use(req, res)
     if (!res.headersSent) res.writeHead(404).end()
   }
